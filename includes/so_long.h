268d70c21b9f0a0f89aa989ef7b546d504552aad
eezn <pplaneoo@gmail.com>
2021-11-25 02:27:57 +0900

    refactor(main.c): Minor changes

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,16 +6,44 @@
 /*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:45:25 by jin-lee           #+#    #+#             */
-/*   Updated: 2021/11/16 15:45:42 by jin-lee          ###   ########.fr       */
+/*   Updated: 2021/11/25 02:20:19 by jin-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	SO_LONG_H
 # define SO_LONG_H
 
+# include <fcntl.h>
+# include <unistd.h>
 # include <stdio.h>
 # include <errno.h>
+# include "./get_next_line.h"
 # include "../mlx/mlx.h"
 # include "../libft/includes/libft.h"
+
+# define MESSAGE 1
+# define PERROR 0
+
+# define TRUE 1
+# define FALSE 0
+
+typedef struct s_game	t_game;
+
+struct	s_game
+{
+	void	*mlx;
+	void	*win;
+};
+
+
+/*
+**Utils
+*/
+void	exit_error(char *message, int flag);
+
+/*
+**
+*/
+void	check_map(char *pathname);
 
 #endif
--- srcs/utils/exit_error.c
+++ srcs/utils/exit_error.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   exit_error.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/16 15:44:33 by jin-lee           #+#    #+#             */
-/*   Updated: 2021/11/25 02:20:29 by jin-lee          ###   ########.fr       */
+/*   Created: 2021/11/16 16:58:24 by jin-lee           #+#    #+#             */
+/*   Updated: 2021/11/25 02:19:37 by jin-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-# define ERR_ARGS "\033[33mUsage: ./so_long <pathname>\033[0m"
-
-int	main(int argc, char **argv)
+void	exit_error(char *message, int flag)
 {
-	t_game	game;
-
-	if (argc != 2)
-		exit_error(ERR_ARGS, MESSAGE);
-	check_map(argv[1]);
-	return (0);
+	if (flag == MESSAGE)
+		ft_putendl_fd(message, STDERR_FILENO);
+	if (flag == PERROR)
+		perror(message);
+	exit(EXIT_FAILURE);
 }
